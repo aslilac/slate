7d64424f4abd27875ecc9e99d761221c3c05b200
McKayla Washburn <mckayla@hey.com>
2022-03-10 20:25:20 -0700

    Use a proper seed

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import { Elm } from "./src/Main.elm";
 
+const DAY = 1000 * 60 * 60 * 24;
+const dayOfGame = Math.floor(Date.now() / DAY);
+
 Elm.Main.init({
 	node: document.querySelector("#app"),
-	flags: { dayOfGame: 69 },
+	flags: { dayOfGame },
 });
